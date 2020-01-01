bbb8fde651ea8e69ff959bdabc5ca9236260d7dd
Ervin Szilagyi <ernyoke19930115@gmail.com>
2020-01-01 18:35:13 +0200

    Fixed usb format script for ntfs and ext4.

--- format-usb.sh
+++ format-usb.sh
@@ -2,6 +2,9 @@
 
 if [ $# -lt 3 ]; then
   echo "Usage: ./format_usb.sh <mountpoint> <format_type (example:vfat)> <name (example: USB-DRIVE)>"
+  echo "Examples:"
+  echo "./format-usb.sh /dev/sda ntfs my-ntfs-usb"
+  echo "./format-usb.sh /dev/sda fat my-fat-usb"
   exit 1
 fi
 
@@ -9,10 +12,30 @@ function format_disk {
   local mount_point=$1
   local format_type=$2
   local drive_name=$3
+  
+  local format_type_arg=''
+
+  echo $format_type
+
+  if [[ $fomat_type == 'ntfs' ]]
+  then
+    format_type_arg='-l'
+  else
+    if [[ $fomat_type == 'vfat' ]] || [[ $format_type == 'fat' ]]
+    then
+      format_type_arg='-n'
+      format_type='vfat'
+    else
+      if [[ $format_type == 'ext4' ]]
+      then
+        format_type_arg='-L'
+      fi
+    fi
+  fi
 
   echo "Starting to format $mount_point $format_type $drive_name"
   umount $mount_point
-  mkfs.$format_type -n $drive_name $mount_point
+  mkfs.$format_type $format_type_arg $drive_name $mount_point
   echo "Finised formatting "$mount_point
 }
 
